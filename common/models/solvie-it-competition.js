149838b01110dd3934cdbcbf259ff900db380d8b
Mikyas <mikidam63@gmail.com>
2018-09-04 17:56:22 +0300

    competition fix

--- common/models/solvie-it-competition.js
+++ common/models/solvie-it-competition.js
@@ -2,4 +2,19 @@
 
 module.exports = function(Solvieitcompetition) {
 
+	Solvieitcompetition.getActiveCompetition = function(cb){
+		Solvieitcompetition.find({ where: { active: true } }, function(err, competition) {
+	  		cb(null, competition);
+	  	});
+	}
+
+	Solvieitcompetition.remoteMethod(
+		'getActiveCompetition',
+		{
+			http: {path: '/active', verb: 'get'},
+			accepts:{arg: 'slung', type: 'string'},
+			returns: {arg: 'Result', type: 'Object'}
+		}
+	);
+
 };
