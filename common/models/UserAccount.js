3b0275a045fdef2bcf61929f244e80f820d4810d
Kaleab Yitbarek <kalzbest@gmail.com>
2018-09-24 12:39:53 +0300

    bug fix

--- common/models/UserAccount.js
+++ common/models/UserAccount.js
@@ -3,190 +3,188 @@ let STATUS = require('../configs/config');
 
 module.exports = function(Useraccount) {
 
-  // validate uniqueness phoneNumber field
-  Useraccount.validatesUniquenessOf("phoneNumber", {message: "Phone Number is not unique."});
+    // validate uniqueness phoneNumber field
+    Useraccount.validatesUniquenessOf("phoneNumber", { message: "Phone Number is not unique." });
 
-  // register SolveIT managment members
-  Useraccount.registerSolveItMgt = async (firstName, middleName, lastName, email, password, phoneNumber) => {
-    let { IcogRole } = Useraccount.app.models;
-    let userRole = await IcogRole.findOne({where: {name: "solve-it-mgt"}});
-    let user = {
-      "firstName": firstName,
-      "middleName": middleName,
-      "lastName": lastName,
-      "phoneNumber": phoneNumber,
-      "password": password,
-      "email": email,
-      "roleId": userRole.id,
-      "created": new Date().toISOString()
-    };
+    // register SolveIT managment members
+    Useraccount.registerSolveItMgt = async(firstName, middleName, lastName, email, password, phoneNumber) => {
+        let { IcogRole } = Useraccount.app.models;
+        let userRole = await IcogRole.findOne({ where: { name: "solve-it-mgt" } });
+        let user = {
+            "firstName": firstName,
+            "middleName": middleName,
+            "lastName": lastName,
+            "phoneNumber": phoneNumber,
+            "password": password,
+            "email": email,
+            "roleId": userRole.id,
+            "created": new Date().toISOString()
+        };
 
-    user = await Useraccount.create(user);
+        user = await Useraccount.create(user);
 
-    return user;
-  }
+        return user;
+    }
 
-  // register SolveIT teams
-  Useraccount.registerSolveItTeam = async (firstName, middleName, lastName, email, password, phoneNumber) => {
-    let { IcogRole } = Useraccount.app.models;
-    let userRole = await IcogRole.findOne({ where : { name: "solve-it-team" } });
+    // register SolveIT teams
+    Useraccount.registerSolveItTeam = async(firstName, middleName, lastName, email, password, phoneNumber) => {
+        let { IcogRole } = Useraccount.app.models;
+        let userRole = await IcogRole.findOne({ where: { name: "solve-it-team" } });
 
-    let user = {
-      "firstName": firstName,
-      "middleName": middleName,
-      "lastName": lastName,
-      "phoneNumber": phoneNumber,
-      "password": password,
-      "email": email,
-      "roleId": userRole.id,
-      "created": new Date().toISOString()
-    };
+        let user = {
+            "firstName": firstName,
+            "middleName": middleName,
+            "lastName": lastName,
+            "phoneNumber": phoneNumber,
+            "password": password,
+            "email": email,
+            "roleId": userRole.id,
+            "created": new Date().toISOString()
+        };
 
-    user = await Useraccount.create(user);
+        user = await Useraccount.create(user);
 
-    return user;
-  }
+        return user;
+    }
 
-  // register SolveIT participants
-  Useraccount.registerParticipants = async (user) => {
-    let { IcogRole } = Useraccount.app.models;
-    let userRole = await IcogRole.findOne({ where : { name: "solve-it-participants" } });
+    // register SolveIT participants
+    Useraccount.registerParticipants = async(user) => {
+        let { IcogRole } = Useraccount.app.models;
+        let userRole = await IcogRole.findOne({ where: { name: "solve-it-participants" } });
 
-    user["roleId"] = userRole.id;
-    user["created"] = new Date().toISOString();
+        user["roleId"] = userRole.id;
+        user["created"] = new Date().toISOString();
 
-    user = await Useraccount.create(user);
+        user = await Useraccount.create(user);
 
-    return user;
-  }
+        return user;
+    }
 
-  // activate registered user
-  Useraccount.activateUser = async (userId) => {
-    let user = await Useraccount.findOne({ where: { id: userId } });
+    // activate registered user
+    Useraccount.activateUser = async(userId) => {
+        let user = await Useraccount.findOne({ where: { id: userId } });
 
-    let updatedUser = await user.updateAttribute('status', STATUS[1]);
-    return user;
-  }
+        let updatedUser = await user.updateAttribute('status', STATUS[1]);
+        return user;
+    }
 
-  Useraccount.deactivateUser = async (userId) => {
-    let user = await Useraccount.findOne({ where: { id: userId } });
+    Useraccount.deactivateUser = async(userId) => {
+        let user = await Useraccount.findOne({ where: { id: userId } });
 
-    let updatedUser = await user.updateAttribute('status', STATUS[0]);
-    return user;
-  }
+        let updatedUser = await user.updateAttribute('status', STATUS[0]);
+        return user;
+    }
 
-  Useraccount.searchUser = function(keyword, cb) {
-    const icogRole = Useraccount.app.models.Icogrole;
-    icogRole.find({where: {name: "solve-it-participants"}}, function(err, role){
-        Useraccount.find({where: {email: {like: keyword}, roleId: role.id}}, function(err, users){
+    Useraccount.searchUser = function(keyword, cb) {
+        const icogRole = Useraccount.app.models.Icogrole;
+        icogRole.find({ where: { name: "solve-it-participants" } }, function(err, role) {
+            Useraccount.find({ where: { email: { like: keyword }, roleId: role.id } }, function(err, users) {
+                cb(null, users);
+            });
+        })
+    }
+
+    Useraccount.getUserListByRole = function(roleId, cb) {
+        Useraccount.find({ where: { roleId: roleId } }, function(err, users) {
             cb(null, users);
         });
-    })
-  }
+    }
 
-  Useraccount.getUserListByRole = function(roleId, cb) {
-    Useraccount.find({where: {roleId: roleId}}, function(err, users){
-      cb(null, users);
+    Useraccount.remoteMethod(
+        'searchUser', {
+            http: { path: '/search/:keyword', verb: 'get' },
+            accepts: { arg: 'keyword', type: 'string' },
+            returns: { arg: 'Result', type: 'Object' }
+        }
+    );
+
+    Useraccount.remoteMethod(
+        'getUserListByRole', {
+            http: { path: '/role/:roleId/users', verb: 'get' },
+            accepts: { arg: 'roleId', type: 'string' },
+            returns: { arg: 'Result', type: 'Object' }
+        }
+    );
+
+    Useraccount.remoteMethod("registerSolveItMgt", {
+        desctiption: "Register SolveIT managment users",
+        accepts: [
+            { arg: "firstName", type: "string", required: true },
+            { arg: "middleName", type: "string", required: true },
+            { arg: "lastName", type: "string", required: true },
+            { arg: "email", type: "string", required: true },
+            { arg: "password", type: "string", required: true },
+            { arg: "phoneNumber", type: "string", required: true }
+        ],
+        http: {
+            verb: "post",
+            path: "/register-solveit-mgt",
+        },
+        returns: {
+            type: "object",
+            root: true
+        }
     });
-  }
 
-  Useraccount.remoteMethod(
-    'searchUser',
-    {
-      http: {path: '/search/:keyword', verb: 'get'},
-      accepts:{arg: 'keyword', type: 'string'},
-      returns: {arg: 'Result', type: 'Object'}
-    }
-  );
+    Useraccount.remoteMethod("registerSolveItTeam", {
+        desctiption: "Register SolveIT teams.",
+        accepts: [
+            { arg: "firstName", type: "string", required: true },
+            { arg: "middleName", type: "string", required: true },
+            { arg: "lastName", type: "string", required: true },
+            { arg: "email", type: "string", required: true },
+            { arg: "password", type: "string", required: true },
+            { arg: "phoneNumber", type: "string", required: true }
+        ],
+        http: {
+            verb: "post",
+            path: "/register-solveit-team",
+        },
+        returns: {
+            type: "object",
+            root: true
+        }
+    });
 
-  Useraccount.remoteMethod(
-    'getUserListByRole',
-    {
-      http: {path: '/role/:roleId/users', verb: 'get'},
-      accepts:{arg: 'roleId', type: 'string'},
-      returns: {arg: 'Result', type: 'Object'}
-    }
-  );
+    Useraccount.remoteMethod("registerParticipants", {
+        desctiption: "Register SolveIT teams.",
+        accepts: [
+            { arg: "user", type: "object", required: true }
+        ],
+        http: {
+            verb: "post",
+            path: "/register-participants",
+        },
+        returns: {
+            type: "object",
+            root: true
+        }
+    });
 
-  Useraccount.remoteMethod("registerSolveItMgt", {
-    desctiption: "Register SolveIT managment users",
-    accepts: [
-      {arg: "firstName", type: "string", required: true},
-      {arg: "middleName", type: "string", required: true},
-      {arg: "lastName", type: "string", required: true},
-      {arg: "email", type: "string", required: true},
-      {arg: "password", type: "string", required: true},
-      {arg: "phoneNumber", type: "string", required: true}
-    ],
-    http: {
-      verb: "post",
-      path: "/register-solveit-mgt",
-    },
-    returns: {
-      type: "object",
-      root: true
-    }
-  });
+    Useraccount.remoteMethod("activateUser", {
+        desctiption: "Activate registered user",
+        accepts: { arg: "userId", type: "string", require: true },
+        http: {
+            verb: "post",
+            path: "/activate-user"
+        },
+        returns: {
+            type: "object",
+            root: true
+        }
+    });
 
-  Useraccount.remoteMethod("registerSolveItTeam", {
-    desctiption: "Register SolveIT teams.",
-    accepts: [
-      {arg: "firstName", type: "string", required: true},
-      {arg: "middleName", type: "string", required: true},
-      {arg: "lastName", type: "string", required: true},
-      {arg: "email", type: "string", required: true},
-      {arg: "password", type: "string", required: true},
-      {arg: "phoneNumber", type: "string", required: true}
-    ],
-    http: {
-      verb: "post",
-      path: "/register-solveit-team",
-    },
-    returns: {
-      type: "object",
-      root: true
-    }
-  });
-
-  Useraccount.remoteMethod("registerParticipants", {
-    desctiption: "Register SolveIT teams.",
-    accepts: [
-      {arg: "user", type: "object", required: true}
-    ],
-    http: {
-      verb: "post",
-      path: "/register-participants",
-    },
-    returns: {
-      type: "object",
-      root: true
-    }
-  });
-
-  Useraccount.remoteMethod("activateUser", {
-    desctiption: "Activate registered user",
-    accepts: {arg: "userId", type: "string", require: true},
-    http: {
-      verb: "post",
-      path: "/activate-user"
-    },
-    returns: {
-      type: "object",
-      root: true
-    }
-  });
-
-  Useraccount.remoteMethod("deactivateUser", {
-    desctiption: "Deactivate registered user",
-    accepts: {arg: "userId", type: "string", require: true},
-    http: {
-      verb: "post",
-      path: "/deactivate-user"
-    },
-    returns: {
-      type: "object",
-      root: true
-    }
-  });
+    Useraccount.remoteMethod("deactivateUser", {
+        desctiption: "Deactivate registered user",
+        accepts: { arg: "userId", type: "string", require: true },
+        http: {
+            verb: "post",
+            path: "/deactivate-user"
+        },
+        returns: {
+            type: "object",
+            root: true
+        }
+    });
 
 };
--- common/models/solveit-discussion.js
+++ common/models/solveit-discussion.js
@@ -2,35 +2,38 @@
 
 module.exports = function(Solveitdiscussion) {
 
-	Solveitdiscussion.getBySlung = function(slung,cb) {
-		Solveitdiscussion.find({ where: { slung: slung } }, function(err, discussion) {
-	  		cb(null, discussion);
-	  	});
-	}
+    Solveitdiscussion.getBySlung = function(slung, cb) {
+        Solveitdiscussion.find({ where: { slung: slung } }, function(err, discussion) {
+            let { UserAccount } = Solveitdiscussion.app.models;
+            UserAccount.findOne({ where: { id: discussion.userAccountId } }, function(err, user) {
+                console.log(user);
+                cb(null, { discussion: discussion, user: user });
+            });
+        });
+    }
 
-	Solveitdiscussion.remoteMethod(
-		'getBySlung',
-		{
-			http: {path: '/:slung/discussion', verb: 'get'},
-			accepts:{arg: 'slung', type: 'string'},
-			returns: {arg: 'Result', type: 'Object'}
-		}
-	);
+    Solveitdiscussion.remoteMethod(
+        'getBySlung', {
+            http: { path: '/:slung/discussion', verb: 'get' },
+            accepts: { arg: 'slung', type: 'string' },
+            returns: { arg: 'Result', type: 'Object' }
+        }
+    );
 
-	Solveitdiscussion.afterRemote('create', function(context, unused, next){
-		var discussionCount = 0;
-		const Forum = Solveitdiscussion.app.models.SolveITForum;
+    Solveitdiscussion.afterRemote('create', function(context, unused, next) {
+        var discussionCount = 0;
+        const Forum = Solveitdiscussion.app.models.SolveITForum;
 
-		Forum.find({where: {id: context.args.data.forumId}}, function(err, forum){
-			discussionCount = forum[0].discussionCount + 1;
-			Forum.updateAll({id: context.args.data.forumId}, {discussionCount: discussionCount}, function(err, count){
-				if (err) {
-			        console.error(err);
-			        next();
-			    }
-			    next();
-			})
-		})
-	});
+        Forum.find({ where: { id: context.args.data.forumId } }, function(err, forum) {
+            discussionCount = forum[0].discussionCount + 1;
+            Forum.updateAll({ id: context.args.data.forumId }, { discussionCount: discussionCount }, function(err, count) {
+                if (err) {
+                    console.error(err);
+                    next();
+                }
+                next();
+            })
+        })
+    });
 
 };
