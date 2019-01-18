8443d2741ac8218fe14384e39008473adb63be4e
Kaleab Yitbarek <kalzbest@gmail.com>
2019-01-18 21:40:58 +0300

    :lock: update UserAccount model acl

--- server/boot/role-resolver.js
+++ server/boot/role-resolver.js
@@ -1,61 +1,60 @@
 /* kal */
 
 module.exports = function(app) {
+  const {IcogRole, UserAccount} = app.models;
 
-    const { IcogRole, UserAccount } = app.models;
-    
-    IcogRole.registerResolver('admin', function(role, context, cb) {
-        console.log(context.accessToken);
-        if (!context.accessToken) {
-            cb(null, false);
+  IcogRole.registerResolver('admin', function(role, context, cb) {
+      console.log(context.accessToken);
+      if (!context.accessToken) {
+          cb(null, false);
         } else {
-            UserAccount.findById(context.accessToken.userId, {include: "role"}, function (error, user) {
-                if (error || !user || user.toJSON().role.name !== "admin"){
-                    cb(null, false);
+          UserAccount.findById(context.accessToken.userId, {include: 'role'}, function(error, user) {
+              if (error || !user || user.toJSON().role.name !== 'admin') {
+                  cb(null, false);
                 } else {
-                    cb(null, true);
+                  cb(null, true);
                 }
             });
         }
     });
 
-    IcogRole.registerResolver('mgt', function(role, context, cb) {
-        if (!context.accessToken) {
-            cb(null, false);
+  IcogRole.registerResolver('mgt', function(role, context, cb) {
+      if (!context.accessToken) {
+          cb(null, false);
         } else {
-            UserAccount.findById(context.accessToken.userId, {include: "role"}, function (error, user) {
-                if (error || !user || user.toJSON().role.name !== "solve-it-mgt"){
-                    cb(null, false);
+          UserAccount.findById(context.accessToken.userId, {include: 'role'}, function(error, user) {
+              if (error || !user || user.toJSON().role.name !== 'solve-it-mgt') {
+                  cb(null, false);
                 } else {
-                    cb(null, true);
+                  cb(null, true);
                 }
             });
         }
     });
 
-    IcogRole.registerResolver('team', function(role, context, cb) {
-        if (!context.accessToken) {
-            cb(null, false);
+  IcogRole.registerResolver('team', function(role, context, cb) {
+      if (!context.accessToken) {
+          cb(null, false);
         } else {
-            UserAccount.findById(context.accessToken.userId, {include: "role"}, function (error, user) {
-                if (error || !user || user.toJSON().role.name !== "solve-it-team"){
-                    cb(null, false);
+          UserAccount.findById(context.accessToken.userId, {include: 'role'}, function(error, user) {
+              if (error || !user || user.toJSON().role.name !== 'solve-it-team') {
+                  cb(null, false);
                 } else {
-                    cb(null, true);
+                  cb(null, true);
                 }
             });
         }
     });
 
-    IcogRole.registerResolver('participant', function(role, context, cb) {
-        if (!context.accessToken) {
-            cb(null, false);
+  IcogRole.registerResolver('participant', function(role, context, cb) {
+      if (!context.accessToken) {
+          cb(null, false);
         } else {
-            UserAccount.findById(context.accessToken.userId, {include: "role"}, function (error, user) {
-                if (error || !user || user.toJSON().role.name !== "solve-it-participants"){
-                    cb(null, false);
+          UserAccount.findById(context.accessToken.userId, {include: 'role'}, function(error, user) {
+              if (error || !user || user.toJSON().role.name !== 'solve-it-participants') {
+                  cb(null, false);
                 } else {
-                    cb(null, true);
+                  cb(null, true);
                 }
             });
         }
