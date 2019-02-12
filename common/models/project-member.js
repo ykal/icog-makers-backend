02ccd3c0e15cbcb3a0e5ca8eb3eefb4da6b2d5c7
Kaleab Yitbarek <kalzbest@gmail.com>
2019-02-12 11:44:24 +0300

    :sparkles: remove project member

--- common/models/project-member.js
+++ common/models/project-member.js
@@ -2,4 +2,28 @@
 
 module.exports = function(Projectmember) {
 
+    Projectmember.removeMember = function(member, cb) {
+        Projectmember.destroyAll({userId: member.userId, projectId: member.projectId}, (error, data) => {
+            if (error) cb(new Error('Error while fetching project member'));
+            return cb(null, data);
+        });
+    }
+
+    Projectmember.remoteMethod('removeMember', {
+        description: "Remove project member",
+        accepts: {
+          arg: "member",
+          type: "object",
+          require: true
+        },
+        http: {
+          verb: "post",
+          path: "/removeMember"
+        },
+        returns: {
+          type: "object",
+          root: true
+        }
+    });
+
 };
