89f5a355ba1e7e59a413205f35a9ff8edf0f4cc8
Kaleab Yitbarek <kalzbest@gmail.com>
2019-03-05 11:40:23 +0300

    :lock: enable delete endpoint on resource model

--- common/models/resource.js
+++ common/models/resource.js
@@ -2,9 +2,4 @@
 
 module.exports = function(Resource) {
 
-    //  disable delete end point
-  Resource.disableRemoteMethod("deleteById", true);
-  Resource.disableRemoteMethod("destroyById", true);
-  Resource.disableRemoteMethod("removeById", true);
-
 };
