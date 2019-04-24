649bf3561dcedf4bfe3b0c834ffb7cdc06706397
Kaleab Yitbarek <kalzbest@gmail.com>
2019-04-24 12:51:01 +0300

    :bug: bug fix on joining competition

--- common/models/competition-project.js
+++ common/models/competition-project.js
@@ -1,10 +1,54 @@
 'use strict';
 
 module.exports = function(Competitionproject) {
-
   //  disable delete end point
-  Competitionproject.disableRemoteMethod("deleteById", true);
-  Competitionproject.disableRemoteMethod("destroyById", true);
-  Competitionproject.disableRemoteMethod("removeById", true);
+  Competitionproject.disableRemoteMethod('deleteById', true);
+  Competitionproject.disableRemoteMethod('destroyById', true);
+  Competitionproject.disableRemoteMethod('removeById', true);
 
+  Competitionproject.enroll = function(
+    competitionId,
+    projectId,
+    questionnaireAnswers,
+    cb
+  ) {
+    const temp = {competitionId, projectId, questionnaireAnswers};
+    Competitionproject.findOrCreate(
+      {where: {and: [{competitionId: competitionId}, {projectId: projectId}]}},
+      temp,
+      (error, response) => {
+        if (error) cb(error);
+        cb(null, response);
+      }
+    );
+  };
+
+  Competitionproject.remoteMethod('enroll', {
+    description: 'Enroll project to competition.',
+    accepts: [
+      {
+        arg: 'competitionId',
+        type: 'string',
+        require: true,
+      },
+      {
+        arg: 'projectId',
+        type: 'string',
+        require: true,
+      },
+      {
+        arg: 'questionnaireAnswers',
+        type: 'object',
+        require: true,
+      },
+    ],
+    http: {
+      verb: 'post',
+      path: '/enroll',
+    },
+    returns: {
+      type: 'object',
+      root: true,
+    },
+  });
 };
--- common/models/solvie-it-competition.js
+++ common/models/solvie-it-competition.js
@@ -1,63 +1,66 @@
 'use strict';
 
 module.exports = function(Solvieitcompetition) {
+  //  disable delete end point
+  Solvieitcompetition.disableRemoteMethod('deleteById', true);
+  Solvieitcompetition.disableRemoteMethod('destroyById', true);
+  Solvieitcompetition.disableRemoteMethod('removeById', true);
 
-	//  disable delete end point
-	Solvieitcompetition.disableRemoteMethod("deleteById", true);
-	Solvieitcompetition.disableRemoteMethod("destroyById", true);
-	Solvieitcompetition.disableRemoteMethod("removeById", true);
-
-	Solvieitcompetition.getActiveCompetition = function(cb){
-		Solvieitcompetition.find({ where: { active: true } }, function(err, competition) {
-	  		cb(null, competition);
-	  	});
+  Solvieitcompetition.getActiveCompetition = function(cb) {
+    Solvieitcompetition.find({where: {active: true}}, function(
+      err,
+      competition
+    ) {
+      cb(null, competition);
+    });
   };
 
-  Solvieitcompetition.getCompetitionProjectsWithCity = async (competitionId) => {
-	const { ProjectMember, CompetitionProject } = Solvieitcompetition.app.models;
+  Solvieitcompetition.getCompetitionProjectsWithCity = async competitionId => {
+    const {ProjectMember, CompetitionProject} = Solvieitcompetition.app.models;
 
-    let competitionProjects = await CompetitionProject.find({where: {competitionId: competitionId},  include: ["solveitproject"]});
+    let competitionProjects = await CompetitionProject.find({
+      where: {competitionId: competitionId},
+      include: ['solveitproject'],
+    });
 
     for (let i = 0; i < competitionProjects.length; i++) {
-        const element = competitionProjects[i];
-        let members = await ProjectMember.find({where: {projectId: element.projectId}, include: ["userAccount"]});
-        members.forEach(member => {
-            if (member.userAccount()) {
-                if (element["cities"]) {
-                    element["cities"] = element["cities"].concat(member.userAccount().cityId);
-                  } else {
-                      element["cities"] = [member.userAccount().cityId];
-                  }
-            }
-        })
-        
+      const element = competitionProjects[i];
+      let members = await ProjectMember.find({
+        where: {projectId: element.projectId},
+        include: ['userAccount'],
+      });
+
+      if (members.length > 0) {
+        let member = members[0];
+        if (member.userAccount()) {
+          element['cities'] = [member.userAccount().cityId];
+        }
+      }
     }
     return competitionProjects;
-}
+  };
 
-Solvieitcompetition.remoteMethod("getCompetitionProjectsWithCity", {
-    desctiption: "get the city of project",
-    accepts: [{
-        arg: "competitionId",
-        type: "string",
-        required: true
-    }],
+  Solvieitcompetition.remoteMethod('getCompetitionProjectsWithCity', {
+    desctiption: 'get the city of project',
+    accepts: [
+      {
+        arg: 'competitionId',
+        type: 'string',
+        required: true,
+      },
+    ],
     http: {
-      verb: "post",
-      path: "/competition-projects"
+      verb: 'post',
+      path: '/competition-projects',
     },
     returns: {
-      type: "object",
-      root: true
-    }
+      type: 'object',
+      root: true,
+    },
   });
 
-	Solvieitcompetition.remoteMethod(
-		'getActiveCompetition',
-		{
-			http: {path: '/active', verb: 'get'},
-			returns: {arg: 'Result', type: 'Object'}
-		}
-	);
-
+  Solvieitcompetition.remoteMethod('getActiveCompetition', {
+    http: {path: '/active', verb: 'get'},
+    returns: {arg: 'Result', type: 'Object'},
+  });
 };
