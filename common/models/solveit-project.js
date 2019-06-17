316447dbfc98cd48fb625315ac9880c50a35afa1
Kaleab Yitbarek <kalzbest@gmail.com>
2019-06-17 11:10:57 +0300

    :recycle: update competition winner model

--- common/models/solveit-project.js
+++ common/models/solveit-project.js
@@ -1,8 +1,8 @@
 'use strict';
 
-module.exports = function(Solveitproject) {
+module.exports = function (Solveitproject) {
 
-    //  disable delete end point
+  //  disable delete end point
   Solveitproject.disableRemoteMethod("deleteById", true);
   Solveitproject.disableRemoteMethod("destroyById", true);
   Solveitproject.disableRemoteMethod("removeById", true);
